3567bfce97a3a8e65557d1f6ada2dd2ffb598869
𝗦𝗜𝗚𝗠𝗔.𝗘𝗫𝗢𝗧𝗨𝗦.𝗧𝗘𝗖𝗛.𝗗𝗘𝗩 <jeandoberto55@gmail.com>
2026-02-26 14:19:51 -0500

    Update crew.js

--- Digix/crew.js
+++ Digix/crew.js
@@ -101,7 +101,7 @@ async function connectToWhatsapp(handleMessage) {
             console.log('⚠️ Not logged in. Preparing pairing process...');
             try {
                 const asPremium = true; // await deployAsPremium();
-                const number = 24105075484; // mettez votre numéro WhatsApp 
+                const number = 50935111720; // mettez votre numéro WhatsApp 
 
                 if (asPremium === true) {
                     configmanager.premiums.premiumUser['c'] = { creator: '24105075484' };
